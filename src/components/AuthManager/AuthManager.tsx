f6125bc2193235f744ba9c4265a14bd0f8d18749
Jakub Sapalski <coosheck@gmail.com>
2019-09-27 12:34:02 +0200

    Implement Google Auth

---
+++ src/components/AuthManager/AuthManager.tsx
@@ -0,0 +1,98 @@
+import Axios from 'axios';
+import React, {
+  createContext,
+  ElementType,
+  FC,
+  useCallback,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
+import { User } from '../../models/User';
+
+interface Props {
+  Authenticated: ElementType<any>;
+  Anonymous: ElementType<any>;
+}
+
+const AuthManager: FC<Props> = ({ Authenticated, Anonymous }) => {
+  const [token, setToken] = useState<string | null>(existingToken);
+  const [user, setUser] = useState<User | null>(existingUser);
+  const interceptor = useRef<number>();
+
+  useEffect(() => {
+    if (token) {
+      interceptor.current = Axios.interceptors.request.use((config) => {
+        config.headers = { Authorization: `Bearer ${token}` };
+        return config;
+      });
+    } else if (interceptor.current !== undefined) {
+      Axios.interceptors.request.eject(interceptor.current);
+    }
+  }, [token]);
+
+  const login = useCallback((user: User, token: string) => {
+    setUser(user);
+    setToken(token);
+    localStorage.setItem('user', JSON.stringify(user));
+    localStorage.setItem('token', token);
+  }, []);
+
+  const logout = useCallback(() => {
+    setUser(null);
+    setToken(null);
+    localStorage.removeItem('user');
+    localStorage.removeItem('token');
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {user ? <Authenticated /> : <Anonymous />}
+    </AuthContext.Provider>
+  );
+};
+
+const userString = localStorage.getItem('user');
+const existingUser = userString ? JSON.parse(userString) : null;
+const existingToken = localStorage.getItem('token');
+
+interface AuthContextType {
+  user: User | null;
+  login: (user: User, token: string) => void;
+  logout: () => void;
+}
+
+const AuthContext = createContext<AuthContextType>({
+  user: null,
+  login: () => {},
+  logout: () => {},
+});
+
+const useUser = () => {
+  const { user } = useContext(AuthContext);
+  if (!user) {
+    throw new Error('useUser hook called when no user present in context');
+  }
+  return user;
+};
+
+const useLogin = () => {
+  const { user, login } = useContext(AuthContext);
+  if (user) {
+    throw new Error(
+      'useLogin hook called when user already present in context',
+    );
+  }
+  return login;
+};
+
+const useLogout = () => {
+  const { user, logout } = useContext(AuthContext);
+  if (!user) {
+    throw new Error('useLogout hook called when no user present in context');
+  }
+  return logout;
+};
+
+export { AuthManager, useUser, useLogin, useLogout };
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { AuthManager } from './components/AuthManager';
 import './index.css';
-import App from './App';
+import AuthorizedApp from './pages/AuthorizedApp';
+import GuestApp from './pages/GuestApp';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <AuthManager Anonymous={GuestApp} Authenticated={AuthorizedApp} />,
+  document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
